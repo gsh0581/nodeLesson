69a0649d288328ab94cc03753469c74a6d10b96b
gsh0581 <503392549@qq.com>
2019-05-10 09:29:40 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,7 @@
+const http = require("http");
+let server = http.createServer((request,response)=>{
+
+});
+server.listen(3000,()=>{
+    console.log('server started at http://localhost:3000');
+});
